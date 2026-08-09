3d056a6e97b74e13f6176e9adece05c802608ad8
gunya999-cmd <gunya999@gmail.com>
2026-08-09 18:17:14 +0300

    Update TargetZone script

--- Assets/Scripts/TargetZone.cs
+++ Assets/Scripts/TargetZone.cs
@@ -2,4 +2,22 @@ using UnityEngine;
 
 public class TargetZone : MonoBehaviour
 {
+    [Tooltip("Тег объекта, который должен попасть в цель. По умолчанию Ball.")]
+    public string targetTag = "Ball";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (string.IsNullOrEmpty(targetTag))
+        {
+            return;
+        }
+
+        if (collision.gameObject.CompareTag(targetTag))
+        {
+            if (GameManager.Instance != null)
+            {
+                GameManager.Instance.CompleteLevel();
+            }
+        }
+    }
 }
